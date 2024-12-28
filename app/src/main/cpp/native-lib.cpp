a5d2f088c190ca075010db5bc380700a38111943
Cendy <cendyemail@gmail.com>
2024-12-28 11:24:52 +0700

    feat: init kittymemory and log some useful stuff

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,17 +1,26 @@
 #include <dlfcn.h>
 #include <thread>
-#include <string>
 #include <android/log.h>
+#include <string>
+#include "vendor/KittyMemory/KittyMemory/KittyInclude.hpp"
 
 const std::string lib_name = "libgrowtopia.so";
+ElfScanner game_lib;
 
 __unused __attribute__((constructor))
 void lib_main() {
     auto thread = std::thread([]() {
         do {
             std::this_thread::sleep_for(std::chrono::seconds(1));
-        } while (!dlopen(lib_name.c_str(), RTLD_NOW));
+            game_lib = ElfScanner::createWithPath(lib_name);
+        } while (!game_lib.isValid());
         __android_log_print(ANDROID_LOG_INFO, "Kuro", "Library %s loaded", lib_name.c_str());
+
+        std::string processName = KittyMemory::getProcessName();
+        void *baseAddress = (void*)game_lib.base();
+        __android_log_print(ANDROID_LOG_INFO, "Kuro", "Process Name: %s", processName.c_str());
+        __android_log_print(ANDROID_LOG_INFO, "Kuro", "Base Address: %p", baseAddress);
+
     });
     thread.detach();
 }
