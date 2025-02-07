26fb7b5b4e2d98ef4e6a2a653b19581bf0a4d651
Cendy <cendyemail@gmail.com>
2025-02-07 16:26:13 +0700

    misc: trying to figure out mod zoom

--- app/src/main/cpp/includes/packet.hpp
+++ app/src/main/cpp/includes/packet.hpp
@@ -2,10 +2,13 @@
 #include <cstdint>
 #include <world.hpp>
 #include <gtitem.h>
+#include "../vendor/enet/include/enet/enet.h"
 
 class Packet {
 public:
-    static void handle(uint8_t *data, bool *should_process_packet, bool log_packet);
+    static void
+    handle(uint8_t *data, bool *should_process_packet, bool log_packet, _ENetPeer *pPeer,
+           int (*pFunction)(ENetPeer *, enet_uint8, ENetPacket *));
 public:
     static World world;
 };
