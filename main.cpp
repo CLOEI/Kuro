16376772ecde13aed1d72d144336b2495155130e
Cendy <cendyemail@gmail.com>
2025-02-27 07:15:46 +0700

    feat: reuse dobby & hook screen width and height

--- main.cpp
+++ main.cpp
@@ -1,61 +1,89 @@
-// #include "dobby.h"
-// #include "thirdparty/KittyMemory/KittyMemory/KittyInclude.hpp"
-// #include "thirdparty/enet/include/enet/enet.h"
-// #include <android/log.h>
-// #include <thread>
+#include <android/log.h>
+#include <thread>
+#include "dobby.h"
+#include "KittyInclude.hpp"
+#include "enet/enet.h"
 
-// auto libName = "libgrowtopia.so";
-// ElfScanner elf;
+auto libName = "libgrowtopia.so";
+ElfScanner elf;
 
-// typedef int32_t (*enet_host_service_t)(int32_t *arg1, int32_t *arg2, int32_t arg3);
-// typedef int (*get_screen_width_t)();
+typedef int (*enet_host_service_t)(ENetHost *host, ENetEvent *event, enet_uint32 timeout);
+typedef int (*get_screen_width_t)();
+typedef int (*get_screen_height_t)();
 
-// enet_host_service_t orig_enet_host_service = nullptr;
-// get_screen_width_t orig_get_screen_width = nullptr;
+enet_host_service_t orig_enet_host_service = nullptr;
+get_screen_width_t orig_get_screen_width = nullptr;
+get_screen_height_t orig_get_screen_height = nullptr;
 
-// int32_t hooked_enet_host_service(int32_t *host, int32_t *event, int32_t timeout) {
-//   __android_log_print(ANDROID_LOG_INFO, "Kuro", "Enet host service called");
-//   return orig_enet_host_service(host, event, timeout);
-// }
+int hooked_enet_host_service(ENetHost *host, ENetEvent *event, enet_uint32 timeout) {
+  switch (event->type)
+  {
+  case ENET_EVENT_TYPE_CONNECT:
+    __android_log_print(ANDROID_LOG_INFO, "Kuro", "Connected to server");
+    break;
+  case ENET_EVENT_TYPE_RECEIVE:
+    __android_log_print(ANDROID_LOG_INFO, "Kuro", "Received data from server");
+    break;
+  case ENET_EVENT_TYPE_DISCONNECT:
+    __android_log_print(ANDROID_LOG_INFO, "Kuro", "Disconnected from server");
+    break;
+  default:
+    break;
+  }
+  return orig_enet_host_service(host, event, timeout);
+}
 
-// int hooked_get_screen_width() {
-//   __android_log_print(ANDROID_LOG_INFO, "Kuro", "Get screen width called");
-//   return orig_get_screen_width();
-// }
+int hooked_get_screen_width() {
+  __android_log_print(ANDROID_LOG_INFO, "Kuro", "Get screen width called");
+  return orig_get_screen_width();
+}
 
-// void hook_function(void *functionAddress, void *hookedFunction, void **originalFunction, const char *functionName) {
-//   if (DobbyHook(functionAddress, hookedFunction, originalFunction) == 0) {
-//     __android_log_print(ANDROID_LOG_INFO, "Kuro", "%s hooked", functionName);
-//   } else {
-//     __android_log_print(ANDROID_LOG_ERROR, "Kuro", "Failed to hook %s",
-//                         functionName);
-//   }
-// }
+int hooked_get_screen_height() {
+  __android_log_print(ANDROID_LOG_INFO, "Kuro", "Get screen height called");
+  return orig_get_screen_height();
+}
+
+void hook_function(void *functionAddress, void *hookedFunction, void **originalFunction, const char *functionName) {
+  if (DobbyHook(functionAddress, hookedFunction, originalFunction) == 0) {
+    __android_log_print(ANDROID_LOG_INFO, "Kuro", "%s hooked", functionName);
+  } else {
+    __android_log_print(ANDROID_LOG_ERROR, "Kuro", "Failed to hook %s", functionName);
+  }
+}
 
 __attribute__((constructor)) void dll_main() {
-  // auto thread = std::thread([]() {
-  //   do {
-  //     sleep(1);
-  //     elf = ElfScanner::createWithPath(libName);
-  //   } while (!elf.isValid());
+  auto thread = std::thread([]() {
+    do {
+      sleep(1);
+      elf = ElfScanner::createWithPath(libName);
+    } while (!elf.isValid());
 
-  //   auto base = elf.base();
-  //   __android_log_print(ANDROID_LOG_INFO, "Kuro", "Base address is %p",
-  //                       (void *)base);
-  //   auto enetHostServiceAddress = base + 0x1085258;
-  //   auto getScreenWidth = base + 0x10A8088;
+    auto base = elf.base();
+    __android_log_print(ANDROID_LOG_INFO, "Kuro", "Base address is %p",
+                        (void *)base);
+    auto enet_host_service = base + 0x1085259;
+    auto get_screen_width = base + 0x10A8089;
+    auto get_screen_height = base + 0x10A8095;
 
-  //   __android_log_print(ANDROID_LOG_INFO, "Kuro",
-  //                       "EnetHostService address is %p",
-  //                       (void *)enetHostServiceAddress);
-  //   __android_log_print(ANDROID_LOG_INFO, "Kuro",
-  //                       "GetScreenWidth address is %p", (void *)getScreenWidth);
+    __android_log_print(ANDROID_LOG_INFO, "Kuro",
+                        "EnetHostService address is %p",
+                        (void *)enet_host_service);
+    __android_log_print(ANDROID_LOG_INFO, "Kuro",
+                        "GetScreenWidth address is %p", (void *)get_screen_width);
+    __android_log_print(ANDROID_LOG_INFO, "Kuro",
+                        "GetScreenHeight address is %p", (void *)get_screen_height);
 
-  //   hook_function((void *)enetHostServiceAddress,
-  //                 (void *)hooked_enet_host_service,
-  //                 (void **)&orig_enet_host_service, "enet_host_service");
-  //   // hook_function((void *)getScreenWidth, (void *)hooked_get_screen_width,
-  //   //               (void **)&orig_get_screen_width, "get_screen_width");
-  // });
-  // thread.detach();
+    hook_function((void *)enet_host_service,
+                  (void *)hooked_enet_host_service,
+                  (void **)&orig_enet_host_service, "enet_host_service");
+
+    hook_function((void *)get_screen_width,
+                  (void *)hooked_get_screen_width,
+                  (void **)&orig_get_screen_width, "get_screen_width");
+    
+    hook_function((void *)get_screen_height,
+                  (void *)hooked_get_screen_height,
+                  (void **)&orig_get_screen_height, "get_screen_height");
+  });
+  thread.detach();
 }
